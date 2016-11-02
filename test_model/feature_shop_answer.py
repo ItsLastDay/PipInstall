5bed2125783f60a83e0f554a55fdf7d3f6e5b1ef
lesya-tishencko <lesya.tishencko2012@yandex.ru>
2016-11-02 19:48:00 +0300

    Fix bag with my features

--- test_model/feature_shop_answer.py
+++ test_model/feature_shop_answer.py
@@ -1,21 +1,15 @@
 
-# coding: utf-8
-
-# In[17]:
-
 def get_features_shop_answer(reviews_array):
     features = []
     for review in reviews_array:
-        features.append(0)
+        features.append([0])
         if len(review['comments']) != 0:
             for comment in review['comments']:
                 if comment['user'].get('name', '').find('Ответ магазина') != -1:
-                    features[len(features) - 1] = 1
+                    features[len(features) - 1][0] = 1
                     break
     return features
 
-
-# In[ ]:
 
 
 
