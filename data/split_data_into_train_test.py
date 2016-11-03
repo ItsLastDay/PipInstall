3e8d99cf68f4ac54dafb88cf4fcaa4b97fe8ba83
Mikhail Koltsov <misha@koltsov.su>
2016-11-03 12:04:51 +0300

    Allow separate generation of cotraining seed data

--- data/split_data_into_train_test.py
+++ data/split_data_into_train_test.py
@@ -4,6 +4,7 @@ import os
 import json
 import os.path
 import random
+import argparse
 
 assessed_results_dir = './assessing_results'
 
@@ -17,6 +18,13 @@ def get_review_data(json_filenames):
     return review_data
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gen-seed-cotraining', action='store_true')
+    parser.add_argument('--split-train-test', action='store_true')
+    args = parser.parse_args()
+    print(args)
+
+
     result_files = list(map(lambda name:
         os.path.abspath(os.path.join(assessed_results_dir, name)), 
         os.listdir(assessed_results_dir)))
@@ -38,14 +46,22 @@ if __name__ == '__main__':
 
     sz_min_set = min(len(truthful_data), len(not_truthful_data))
 
-    # Put 50 reviews of each class into Test set (evaluated in the end)
-    with open('./test_reviews_good.json', 'w') as test_reviews_good:
-        json.dump(truthful_data[:50], test_reviews_good)
-    with open('./test_reviews_paid.json', 'w') as test_reviews_paid:
-        json.dump(not_truthful_data[:50], test_reviews_paid)
+    if args.split_train_test:
+        # Put 50 reviews of each class into Test set (evaluated in the end)
+        with open('./test_reviews_good.json', 'w') as test_reviews_good:
+            json.dump(truthful_data[:50], test_reviews_good)
+        with open('./test_reviews_paid.json', 'w') as test_reviews_paid:
+            json.dump(not_truthful_data[:50], test_reviews_paid)
 
-    # Put `sz_min_set` - 50 reviews of each class into Training set
-    with open('./reviews_good.json', 'w') as train_reviews_good:
-        json.dump(truthful_data[-(sz_min_set - 50):], train_reviews_good)
-    with open('./reviews_paid.json', 'w') as train_reviews_paid:
-        json.dump(not_truthful_data[-(sz_min_set - 50):], train_reviews_paid)
+        # Put `sz_min_set` - 50 reviews of each class into Training set
+        with open('./reviews_good.json', 'w') as train_reviews_good:
+            json.dump(truthful_data[-(sz_min_set - 50):], train_reviews_good)
+        with open('./reviews_paid.json', 'w') as train_reviews_paid:
+            json.dump(not_truthful_data[-(sz_min_set - 50):], train_reviews_paid)
+
+    if args.gen_seed_cotraining:
+        # Dump seed reviews for cotraining. 
+        with open('./cotraining_data/seed_good.json', 'w') as seed_good:
+            json.dump(truthful_data[:sz_min_set], seed_good)
+        with open('./cotraining_data/seed_paid.json', 'w') as seed_paid:
+            json.dump(not_truthful_data[:sz_min_set], seed_paid)
