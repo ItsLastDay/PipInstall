a44f13083c4e67a614e36ef3f6a9f3daa07d5b77
lesya-tishencko <lesya.tishencko2012@yandex.ru>
2016-11-03 13:02:13 +0300

    Changed synonims dictionary

--- test_model/feature_extractors/feature_synonim.py
+++ test_model/feature_extractors/feature_synonim.py
@@ -7,9 +7,9 @@ syn_dict_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
 synonim_dict = open(syn_dict_path).read().split('\n')
 syn_dict = {}
 for it in synonim_dict:
-    string = it.split('\t')
-    if len(string) == 2:
-        syn_dict[string[0]] = string[1].split(' ')
+    string = it.split('|')
+    if len(string) >= 2:
+        syn_dict[string[0]] = string[1:len(string)]
     
 def get_features_synonim(reviews_array):
     features = []
