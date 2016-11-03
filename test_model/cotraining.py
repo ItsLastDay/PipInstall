64c6506e9b342962c282940b8da4c18919ebc10a
Mikhail Koltsov <misha@koltsov.su>
2016-11-03 15:17:26 +0300

    Fix bugs in cotraining, add preliminary results

--- test_model/cotraining.py
+++ test_model/cotraining.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import json
-import numpy
+import numpy as np
 
 from load_data import load_reviews
 from feature_extractors import *
@@ -9,25 +9,36 @@ from test_classifier import compute_features
 
 import sklearn.naive_bayes
 
-MIN_PROBA = 0.9
+MIN_PROBA = 0.95
 COTRAIN_FOLDER = '../data/cotraining_data'
 
 if __name__ == '__main__':
-    seed_data = load_reivews(folder=COTRAIN_FOLDER, prefix='seed')
+    print('Welcome to cotraining!')
 
-    raw_data = json.load('{}/raw_data.json'.format(COTRAIN_FOLDER))
+    seed_data = load_reviews(folder=COTRAIN_FOLDER, prefix='seed')
+
+    raw_data = json.load(open('{}/raw_data.json'.format(COTRAIN_FOLDER), 'r'))
     # Only results with grade >= 4 (which is >= 1 in -2..2). 
     raw_data = list(filter(lambda x: x['grade'] >= 1, raw_data))
     raw_data = np.array(raw_data)
 
-    cur_data_good = np.array(seed_data['good'])
-    cur_data_paid = np.array(seed_data['paid'])
+    cur_data_good = seed_data['good']
+    cur_data_paid = seed_data['paid']
 
+    print('Raw data has length {}'.format(len(raw_data)))
+
+    number_of_added_good_so_far = 0
+    number_of_added_paid_so_far = 0
+
+    it = 0
     while True:
+        print('\n\nIteration {}'.format(it))
         cls_review_centric = sklearn.naive_bayes.MultinomialNB()
         cls_user_centric = sklearn.naive_bayes.GaussianNB()
 
         flat_reviews = cur_data_good + cur_data_paid
+        flat_reviews = np.array(flat_reviews)
+
         labels = [0 for i in range(len(cur_data_good))] +\
                 [1 for i in range(len(cur_data_paid))]
 
@@ -36,10 +47,10 @@ if __name__ == '__main__':
 
         # Train two classifiers on distinc features.
         train_features_review_centric = compute_features(flat_reviews,
-                [feature_words_vector, feature_mean_len_word,
-                    feature_parts_of_speech])
+                [feature_words_vector,
+                    get_features_number_exclamation])
         train_features_user_centric = compute_features(flat_reviews,
-                [feature_meta])
+                [get_features_meta])
 
         cls_review_centric.fit(train_features_review_centric, labels)
         cls_user_centric.fit(train_features_user_centric, labels)
@@ -48,10 +59,10 @@ if __name__ == '__main__':
         # Get features for raw data.
         feature_words_vector = lambda x: words_vector(x, False)
         test_features_review_centric = compute_features(raw_data,
-                [feature_words_vector, feature_mean_len_word,
-                    feature_parts_of_speech])
+                [feature_words_vector,
+                    get_features_number_exclamation])
         test_features_user_centric = compute_features(raw_data,
-                [feature_meta])
+                [get_features_meta])
 
         raw_proba_review_centric = cls_review_centric.predict_proba(test_features_review_centric)
         raw_proba_user_centric = cls_user_centric.predict_proba(test_features_user_centric)
@@ -64,24 +75,32 @@ if __name__ == '__main__':
                 raw_proba_user_centric[i][1] > MIN_PROBA]
         print('Number of new certain good reviews: {}'.format(len(certain_good_indices)))
         print('Number of new certain paid reviews: {}'.format(len(certain_paid_indices)))
+        #print([(i, raw_proba_review_centric[i]) for i in certain_good_indices])
+
+        number_of_added_good_so_far += len(certain_good_indices)
+        number_of_added_paid_so_far += len(certain_paid_indices)
 
         new_good_reviews = raw_data[certain_good_indices]
         new_paid_reviews = raw_data[certain_paid_indices]
 
-        mask = numpy.ma.array(raw_data, mask=True)
-        mask[certain_good_indices] = False
-        mask[certain_paid_indices] = False
-        raw_data = raw_data[mask]
+        raw_data = [x for i, x in enumerate(raw_data) if i not in certain_good_indices 
+                and i not in certain_paid_indices]
+        raw_data = np.array(raw_data)
+        print('Now raw_data is of length {}'.format(len(raw_data)))
 
-        cur_data_good += new_good_reviews
-        cur_data_paid += new_paid_reviews
+        cur_data_good.extend(new_good_reviews)
+        cur_data_paid.extend(new_paid_reviews)
 
-        if len(certain_good_indices) + len(certain_paid_indices) == 0:
+        print('Number of added good so far: {}'.format(number_of_added_good_so_far))
+        print('Number of added paid so far: {}'.format(number_of_added_paid_so_far))
+
+        if len(certain_good_indices) + len(certain_paid_indices) == 0 or it == 3:
             break
+        it += 1
 
 
-    with open('{}/result_data_good.json', 'w') as result_good:
+    with open('{}/result_data_good.json'.format(COTRAIN_FOLDER), 'w') as result_good:
         json.dump(cur_data_good, result_good)
 
-    with open('{}/result_data_paid.json', 'w') as result_paid:
+    with open('{}/result_data_paid.json'.format(COTRAIN_FOLDER), 'w') as result_paid:
         json.dump(cur_data_paid, result_paid)
