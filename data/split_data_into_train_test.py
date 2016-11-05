243b824cf1879f87c1c56ec946b8c97f9eb9c9d1
Mikhail Koltsov <misha@koltsov.su>
2016-11-05 15:58:20 +0300

    Refactor splitting into train\test: specify size of test

--- data/split_data_into_train_test.py
+++ data/split_data_into_train_test.py
@@ -17,12 +17,9 @@ def get_review_data(json_filenames):
 
     return review_data
 
-TEST_SIZE = 150
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gen-seed-cotraining', action='store_true')
-    parser.add_argument('--split-train-test', action='store_true')
+    parser.add_argument('test_size', type=int)
     args = parser.parse_args()
 
 
@@ -36,6 +33,8 @@ if __name__ == '__main__':
             'merged_bad.txt' in x,
             result_files))
 
+    TEST_SIZE = args.test_size
+
     truthful_data = get_review_data(truthful_names)
     not_truthful_data = get_review_data(not_truthful_names)
 
@@ -47,32 +46,17 @@ if __name__ == '__main__':
 
     sz_min_set = min(len(truthful_data), len(not_truthful_data))
 
-    if args.split_train_test:
-        # Put TEST_SIZE reviews of each class into Test set (evaluated in the end)
-        with open('./test_reviews_good.json', 'w') as test_reviews_good:
-            json.dump(truthful_data[:TEST_SIZE], test_reviews_good)
-        with open('./test_reviews_paid.json', 'w') as test_reviews_paid:
-            json.dump(not_truthful_data[:TEST_SIZE], test_reviews_paid)
+    # Put TEST_SIZE reviews of each class into Test set (evaluated in the end)
+    with open('./test_reviews_good.json', 'w') as test_reviews_good:
+        json.dump(truthful_data[:TEST_SIZE], test_reviews_good)
+    with open('./test_reviews_paid.json', 'w') as test_reviews_paid:
+        json.dump(not_truthful_data[:TEST_SIZE], test_reviews_paid)
 
-        # Put `sz_min_set` - TEST_SIZE reviews of each class into Training set
-        with open('./reviews_good.json', 'w') as train_reviews_good:
-            json.dump(truthful_data[-(sz_min_set - TEST_SIZE):], train_reviews_good)
-        with open('./reviews_paid.json', 'w') as train_reviews_paid:
-            json.dump(not_truthful_data[-(sz_min_set - TEST_SIZE):], train_reviews_paid)
-
-    if args.gen_seed_cotraining:
-        print('ENTER COTRAINING')
-        # Put TEST_SIZE reviews of each class into Test set (evaluated in the end)
-        with open('./test_reviews_good.json', 'w') as test_reviews_good:
-            json.dump(truthful_data[:TEST_SIZE], test_reviews_good)
-        with open('./test_reviews_paid.json', 'w') as test_reviews_paid:
-            json.dump(not_truthful_data[:TEST_SIZE], test_reviews_paid)
-
-        # Dump seed reviews for cotraining, use all remaining data. 
-        with open('./cotraining_data/seed_good.json', 'w') as seed_good:
-            json.dump(truthful_data[TEST_SIZE:], seed_good)
-        with open('./cotraining_data/seed_paid.json', 'w') as seed_paid:
-            json.dump(not_truthful_data[TEST_SIZE:], seed_paid)
+    # Dump seed reviews for cotraining, use all remaining data. 
+    with open('./cotraining_data/seed_good.json', 'w') as seed_good:
+        json.dump(truthful_data[TEST_SIZE:], seed_good)
+    with open('./cotraining_data/seed_paid.json', 'w') as seed_paid:
+        json.dump(not_truthful_data[TEST_SIZE:], seed_paid)
 
 
 
